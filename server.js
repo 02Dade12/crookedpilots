a8f94b81ccd95a4887793ba72f6ea970320db5bf
Darius Hay <dariushay@gmail.com>
2021-05-11 21:44:14 -0400

    mysql connected

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
-var compression = require('compression')
+// const routes = require('./controllers');
+// var compression = require('compression')
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(compression());
+// app.use(compression());
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -28,9 +28,11 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client"));
+}
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
